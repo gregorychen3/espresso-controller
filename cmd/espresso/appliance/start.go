02af2d757670e5ea26664e50e4a558f58bdf0038
Gregory Chen <gregorychen3@gmail.com>
2020-04-09 16:53:04 -0400

    start appliance server from cli

--- cmd/espresso/appliance/start.go
+++ cmd/espresso/appliance/start.go
@@ -1,9 +1,10 @@
 package appliance
 
 import (
-	"log"
-
+	"github.com/gregorychen3/espresso-controller/cmd/espresso/log"
+	"github.com/gregorychen3/espresso-controller/internal/appliance"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func NewApplianceStartCmd() *cobra.Command {
@@ -12,7 +13,19 @@ func NewApplianceStartCmd() *cobra.Command {
 		Short: "Start an espresso making appliance",
 		Long:  "Start an espresso making appliance",
 		Run: func(cmd *cobra.Command, args []string) {
-			log.Fatal("implement me")
+			c := appliance.Configuration{}
+			if err := viper.Unmarshal(&c); err != nil {
+				log.Fatal("Unmarshalling configuration: %s\n", err.Error())
+			}
+
+			server, err := appliance.New(c)
+			if err != nil {
+				log.Fatal("Failed to create server: %s\n", err.Error())
+			}
+
+			if err := server.Run(); err != nil {
+				log.Fatal("Failed to run server: %s", err.Error())
+			}
 		},
 	}
 
--- go.mod
+++ go.mod
@@ -3,12 +3,13 @@ module github.com/gregorychen3/espresso-controller
 go 1.14
 
 require (
-	github.com/golang/protobuf v1.3.5 // indirect
+	github.com/golang/protobuf v1.3.5
 	github.com/spf13/cobra v0.0.7
+	github.com/spf13/viper v1.4.0
 	go.uber.org/zap v1.10.0
 	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
 	golang.org/x/sys v0.0.0-20200409092240-59c9f1ba88fa // indirect
 	golang.org/x/text v0.3.2 // indirect
 	google.golang.org/genproto v0.0.0-20200409111301-baae70f3302d // indirect
-	google.golang.org/grpc v1.28.1 // indirect
+	google.golang.org/grpc v1.28.1
 )
