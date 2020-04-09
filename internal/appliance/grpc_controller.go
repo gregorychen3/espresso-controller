a40cb353d5eb4de155033d8fa413f976831ba03c
Gregory Chen <gregorychen3@gmail.com>
2020-04-09 15:26:17 -0400

    implement Appliance interface in grpc controller

--- internal/appliance/grpc_controller.go
+++ internal/appliance/grpc_controller.go
@@ -1,9 +1,27 @@
 package appliance
 
+import (
+	"context"
+
+	"github.com/gregorychen3/espresso-controller/pkg/appliancepb"
+)
+
 type grpcController struct {
 	c Configuration
 }
 
 func newGrpcController(c Configuration) *grpcController {
 	return &grpcController{c: c}
+}
+
+func (c *grpcController) GetCurrentTemperature(context.Context, *appliancepb.GetCurrentTemperatureRequest) (*appliancepb.GetCurrentTemperatureResponse, error) {
+	return &appliancepb.GetCurrentTemperatureResponse{}, nil
+}
+
+func (c *grpcController) GetTargetTemperature(context.Context, *appliancepb.GetTargetTemperatureRequest) (*appliancepb.GetTargetTemperatureResponse, error) {
+	return &appliancepb.GetTargetTemperatureResponse{}, nil
+}
+
+func (c *grpcController) SetTargetTemperature(context.Context, *appliancepb.SetTargetTemperatureRequest) (*appliancepb.SetTargetTemperatureResponse, error) {
+	return &appliancepb.SetTargetTemperatureResponse{}, nil
 }
