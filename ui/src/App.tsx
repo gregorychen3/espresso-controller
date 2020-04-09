567bff7e618c68890be8d875d2a9a9d614e0c842
Gregory Chen <gregorychen3@gmail.com>
2020-04-09 17:21:31 -0400

    material ui hello world

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,12 @@
+import Button from "@material-ui/core/Button";
 import React from "react";
 import "./App.css";
+import "typeface-roboto";
 
 export default () => {
-  return <div>Hello World</div>;
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
 };
