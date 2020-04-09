0b0ab36c224593c90b93508cd3964e1809e0371e
Gregory Chen <gregorychen3@gmail.com>
2020-04-09 11:37:25 -0400

    add root command

--- cmd/espresso/main.go
+++ cmd/espresso/main.go
@@ -3,7 +3,17 @@ package main
 import "github.com/spf13/cobra"
 
 func newRootCmd() *cobra.Command {
-	cmd := cobra.Command{}
+	cmd := cobra.Command{
+		Use:   "espresso",
+		Short: "Control and monitor an espresso machine",
+		Long:  "Control and monitor an espresso machine",
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := cmd.Help(); err != nil {
+				println("Failed to output help for command")
+			}
+		},
+	}
+
 	return &cmd
 }
 
