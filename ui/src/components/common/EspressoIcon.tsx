aa7dcc214c6a053d47132abdbca2d13511df226c
Gregory Chen <gregorychen3@gmail.com>
2020-04-09 22:30:30 -0400

    add missing return in espresso icon

--- ui/src/components/common/EspressoIcon.tsx
+++ ui/src/components/common/EspressoIcon.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { SvgIcon } from "@material-ui/core";
 
 export default (props: any) => {
-  <SvgIcon {...props}>
-    <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-  </SvgIcon>;
+  return (
+    <SvgIcon {...props}>
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </SvgIcon>
+  );
 };
