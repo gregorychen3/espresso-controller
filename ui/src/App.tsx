46808e0dfe78d2cedc26fc1eb469442306b2c216
Gregory Chen <gregorychen3@gmail.com>
2020-04-10 00:09:54 -0400

    wire up copyright

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -27,11 +27,10 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+      <Link color="inherit" href="http://www.gregory-chen.com/">
+        Gregory Chen
       </Link>{" "}
       {new Date().getFullYear()}
-      {"."}
     </Typography>
   );
 }
