a287bc83e6d0220fb0a1f6301698b738da6dcba1
Gregory Chen <gregorychen3@gmail.com>
2020-04-10 17:18:28 -0400

    separate serveGRPC and serveHTTP1 funcs

--- go.mod
+++ go.mod
@@ -4,12 +4,16 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.3.5
+	github.com/pkg/errors v0.8.0
+	github.com/soheilhy/cmux v0.1.4
 	github.com/spf13/cobra v0.0.7
 	github.com/spf13/viper v1.4.0
 	go.uber.org/zap v1.10.0
 	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58
 	golang.org/x/sys v0.0.0-20200409092240-59c9f1ba88fa // indirect
 	golang.org/x/text v0.3.2 // indirect
 	google.golang.org/genproto v0.0.0-20200409111301-baae70f3302d // indirect
 	google.golang.org/grpc v1.28.1
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 )
--- internal/appliance/server.go
+++ internal/appliance/server.go
@@ -2,10 +2,14 @@ package appliance
 
 import (
 	"fmt"
-	"log"
 	"net"
 
+	"github.com/gregorychen3/espresso-controller/internal/log"
 	"github.com/gregorychen3/espresso-controller/pkg/appliancepb"
+	"github.com/pkg/errors"
+	"github.com/soheilhy/cmux"
+	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 )
 
@@ -32,8 +36,34 @@ func (s *Server) serveTCP() error {
 
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", s.c.Port))
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		return errors.Wrap(err, fmt.Sprintf("failed to listen on port %d", s.c.Port))
 	}
 
-	return grpcServer.Serve(listener)
+	mux := cmux.New(listener)
+	grpcListener := mux.MatchWithWriters(cmux.HTTP2MatchHeaderFieldPrefixSendSettings("content-type", "application/grpc"))
+	//http1Listener := mux.Match(cmux.HTTP1())
+
+	eg := errgroup.Group{}
+	eg.Go(func() error { return s.serveGRPC(grpcListener, grpcServer) })
+	//eg.Go(func() error { return s.serveHTTP1(http1Listener, grpcServer) })
+	eg.Go(func() error { return mux.Serve() })
+	if err := eg.Wait(); err != nil {
+		return err
+	}
+	return nil
+	//	return grpcServer.Serve(listener)
+}
+
+func (s *Server) serveGRPC(listener net.Listener, grpcServer *grpc.Server) error {
+	log.Info("Initializing gRPC server", zap.Int("port", s.c.Port))
+	if err := grpcServer.Serve(listener); err != nil {
+		return errors.Wrap(err, "gRPC server failed")
+	}
+	return nil
+}
+
+func (s *Server) serveHTTP1(listener net.Listener, grpcServer *grpc.Server) error {
+	log.Info("Initializing http1 web server", zap.Int("port", s.c.Port))
+	// TODO
+	return nil
 }
