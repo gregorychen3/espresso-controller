dea6cf2cdbe22491a6c0f3121b174a5cf7aa8c54
Gregory Chen <gregorychen3@gmail.com>
2020-04-10 00:18:56 -0400

    update strings in chart

--- ui/src/components/Chart.tsx
+++ ui/src/components/Chart.tsx
@@ -32,7 +32,7 @@ export default function Chart() {
 
   return (
     <React.Fragment>
-      <Title>Today</Title>
+      <Title>Temperature Over Time</Title>
       <ResponsiveContainer>
         <LineChart
           data={data}
@@ -50,7 +50,7 @@ export default function Chart() {
               position="left"
               style={{ textAnchor: "middle", fill: theme.palette.text.primary }}
             >
-              Sales ($)
+              Temperature (°C)
             </Label>
           </YAxis>
           <Line
