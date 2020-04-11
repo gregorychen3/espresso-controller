649c535a68a3a2b60bb74cb9a43fc9510aca360a
Gregory Chen <gregorychen3@gmail.com>
2020-04-11 14:27:01 -0400

    round temp to 1 decimal

--- ui/src/components/Temperature.tsx
+++ ui/src/components/Temperature.tsx
@@ -29,7 +29,7 @@ export default () => {
     <>
       <Title>Current Temperature</Title>
       <Typography component="p" variant="h4">
-        {curTemp?.temperature ?? "--"} °C
+        {curTemp?.temperature.toFixed(1) ?? "--"} °C
       </Typography>
       {curTemp && (
         <Typography
