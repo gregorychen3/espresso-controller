c07dfcc5bdd6571ad24c9aa2b98b4b0f38eb7222
Gregory Chen <gregorychen3@gmail.com>
2020-04-11 14:14:54 -0400

    wire up chart with real data

--- ui/src/components/TemperatureOverTime.tsx
+++ ui/src/components/TemperatureOverTime.tsx
@@ -1,33 +1,25 @@
-import React from "react";
 import { useTheme } from "@material-ui/core/styles";
+import React from "react";
+import { useSelector } from "react-redux";
 import {
-  LineChart,
+  Label,
   Line,
+  LineChart,
+  ResponsiveContainer,
   XAxis,
   YAxis,
-  Label,
-  ResponsiveContainer,
 } from "recharts";
+import { getTempHistory } from "../redux/selectors";
 import Title from "./Title";
-
-function createData(time: string, amount: number | undefined) {
-  return { time, amount };
-}
-
-const data = [
-  createData("00:00", 0),
-  createData("03:00", 300),
-  createData("06:00", 600),
-  createData("09:00", 800),
-  createData("12:00", 1500),
-  createData("15:00", 2000),
-  createData("18:00", 2400),
-  createData("21:00", 2400),
-  createData("24:00", undefined),
-];
 
 export default () => {
   const theme = useTheme();
+
+  const samples = useSelector(getTempHistory);
+  const data = samples.map((s) => ({
+    time: s.observedAt.format("h:m:ssa"),
+    temp: s.temperature,
+  }));
 
   return (
     <>
@@ -54,7 +46,7 @@ export default () => {
           </YAxis>
           <Line
             type="monotone"
-            dataKey="amount"
+            dataKey="temp"
             stroke={theme.palette.primary.main}
             dot={false}
           />
