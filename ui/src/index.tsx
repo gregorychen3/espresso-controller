bd90ed03709d69c7a3e825d83ebb043e3ebd653d
Gregory Chen <gregorychen3@gmail.com>
2020-04-10 20:05:34 -0400

    use redux provider

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { Provider } from "react-redux";
 import App from "./App";
+import "./index.css";
+import { store } from "./redux";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
