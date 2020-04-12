709af658f07065ef9c0e943e6ba78d0b421d7be9
Gregory Chen <gregorychen3@gmail.com>
2020-04-12 18:09:57 -0400

    add ControlStrategy interface

--- pkg/control/control.go
+++ pkg/control/control.go
@@ -2,6 +2,13 @@ package control
 
 import "time"
 
+type ControlStrategy interface {
+	GetCurrentTemperature() TemperatureSample
+	GetTemperatureHistory() []TemperatureSample
+	GetSetPoint() SetPoint
+	SetSetPoint(temperature float32) SetPoint
+}
+
 type SetPoint struct {
 	Temperature float32
 	SetAt       time.Time
