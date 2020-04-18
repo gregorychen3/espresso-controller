6241eba4a584c69beea472297c1c4622be0d4276
Gregory Chen <gregorychen3@gmail.com>
2020-04-18 19:15:44 -0400

    add raspi gpu temp to system metrics

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -1,6 +1,8 @@
 package metrics
 
 import (
+	"os/exec"
+	"strconv"
 	"time"
 
 	"github.com/armon/go-metrics"
@@ -12,8 +14,9 @@ import (
 )
 
 var (
-	metricKeyMemUtilizationRatio = []string{"node_mem_utilization_ratio"}
-	metricKeyCPUUtilizationRatio = []string{"node_cpu_utilization_ratio"}
+	metricKeyMemUtilizationRatio = []string{"raspi_mem_utilization_ratio"}
+	metricKeyCPUUtilizationRatio = []string{"raspi_cpu_utilization_ratio"}
+	metricKeyRaspiGPUTemperature = []string{"raspi_gpu_temperature"}
 )
 
 // InitMetrics creates a prometheus sink
@@ -37,13 +40,36 @@ func InitMetrics() error {
 func CollectSystemMetrics() {
 	memStats, err := mem.VirtualMemory()
 	if err != nil {
-		log.Error("Collecting system memory utilization metrics", zap.Error(err))
+		log.Error("Failed to collect system memory utilization metrics", zap.Error(err))
+	} else {
+		metrics.SetGauge(metricKeyMemUtilizationRatio, float32(memStats.UsedPercent/100))
 	}
-	metrics.SetGauge(metricKeyMemUtilizationRatio, float32(memStats.UsedPercent/100))
 
 	cpuStats, err := cpu.Percent(0, false)
 	if err != nil {
-		log.Error("Collecting system cpu utilization metrics", zap.Error(err))
+		log.Error("Failed to collect system cpu utilization metrics", zap.Error(err))
+	} else {
+		metrics.SetGauge(metricKeyCPUUtilizationRatio, float32(cpuStats[0]/100))
 	}
-	metrics.SetGauge(metricKeyCPUUtilizationRatio, float32(cpuStats[0]/100))
+
+	gpuTemperature, err := sampleRaspiGPUTemperature()
+	if err != nil {
+		log.Error("Failed to sample raspi gpu temperature", zap.Error(err))
+	} else {
+		metrics.SetGauge(metricKeyCPUUtilizationRatio, gpuTemperature)
+	}
+}
+
+func sampleRaspiGPUTemperature() (float32, error) {
+	outBytes, err := exec.Command("vcgencmd measure_temp").Output()
+	if err != nil {
+		return 0, nil
+	}
+
+	temperature64, err := strconv.ParseFloat(string(outBytes), 32)
+	if err != nil {
+		return 0, err
+	}
+
+	return float32(temperature64), nil
 }
