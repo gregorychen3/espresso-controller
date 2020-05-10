9ced7a441888e632ee3dd588cf91b8572ff82612
Gregory Chen <gregorychen3@gmail.com>
2020-05-10 17:41:24 -0400

    NewMax31855() take spiDeviceNum param

--- internal/appliance/temperature/max31855/max31855.go
+++ internal/appliance/temperature/max31855/max31855.go
@@ -1,6 +1,8 @@
 package max31855
 
 import (
+	"fmt"
+
 	"github.com/gregorychen3/espresso-controller/internal/appliance/temperature"
 	"github.com/teebr/go-max31855"
 )
@@ -9,9 +11,10 @@ type Max31855 struct {
 	sensor max31855.MAX31855
 }
 
-func NewMax31855() (*Max31855, error) {
+func NewMax31855(spiDeviceNum int) (*Max31855, error) {
 	var sensor max31855.MAX31855
-	if err := sensor.Open("/dev/spidev0.0"); err != nil {
+	path := fmt.Sprintf("/dev/spidev0.%d", spiDeviceNum)
+	if err := sensor.Open(path); err != nil {
 		return nil, err
 	}
 	return &Max31855{sensor: sensor}, nil
