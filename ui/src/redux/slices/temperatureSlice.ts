7c62c2ac5b629e2af8c358ed67c47f040dc1b077
Gregory Chen <gregorychen3@gmail.com>
2020-05-11 11:56:35 -0400

    improve toast message

--- ui/src/redux/slices/temperatureSlice.ts
+++ ui/src/redux/slices/temperatureSlice.ts
@@ -107,7 +107,7 @@ export const startBoilerTemperatureStream = (req: TemperatureStreamRequest) => (
     }
   });
   stream.on("end", (status) => {
-    toast.error(`Error: stream ended with status: ${status?.details}`);
+    toast.error(`Error: boiler temperature stream ended: ${status?.details}`);
   });
 };
 
