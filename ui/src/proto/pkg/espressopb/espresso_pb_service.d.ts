95103b9afcddc5fc4a4b9689d4f5e3b55f9db1d0
Gregory Chen <gregorychen3@gmail.com>
2020-05-24 12:18:40 -0400

    make proto

---
+++ ui/src/proto/pkg/espressopb/espresso_pb_service.d.ts
@@ -0,0 +1,104 @@
+// package: espressopb
+// file: pkg/espressopb/espresso.proto
+
+import * as pkg_espressopb_espresso_pb from "../../pkg/espressopb/espresso_pb";
+import {grpc} from "@improbable-eng/grpc-web";
+
+type EspressoGroupHeadTemperature = {
+  readonly methodName: string;
+  readonly service: typeof Espresso;
+  readonly requestStream: false;
+  readonly responseStream: true;
+  readonly requestType: typeof pkg_espressopb_espresso_pb.TemperatureStreamRequest;
+  readonly responseType: typeof pkg_espressopb_espresso_pb.TemperatureStreamResponse;
+};
+
+type EspressoBoilerTemperature = {
+  readonly methodName: string;
+  readonly service: typeof Espresso;
+  readonly requestStream: false;
+  readonly responseStream: true;
+  readonly requestType: typeof pkg_espressopb_espresso_pb.TemperatureStreamRequest;
+  readonly responseType: typeof pkg_espressopb_espresso_pb.TemperatureStreamResponse;
+};
+
+type EspressoGetTargetTemperature = {
+  readonly methodName: string;
+  readonly service: typeof Espresso;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof pkg_espressopb_espresso_pb.GetTargetTemperatureRequest;
+  readonly responseType: typeof pkg_espressopb_espresso_pb.GetTargetTemperatureResponse;
+};
+
+type EspressoSetTargetTemperature = {
+  readonly methodName: string;
+  readonly service: typeof Espresso;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof pkg_espressopb_espresso_pb.SetTargetTemperatureRequest;
+  readonly responseType: typeof pkg_espressopb_espresso_pb.SetTargetTemperatureResponse;
+};
+
+export class Espresso {
+  static readonly serviceName: string;
+  static readonly GroupHeadTemperature: EspressoGroupHeadTemperature;
+  static readonly BoilerTemperature: EspressoBoilerTemperature;
+  static readonly GetTargetTemperature: EspressoGetTargetTemperature;
+  static readonly SetTargetTemperature: EspressoSetTargetTemperature;
+}
+
+export type ServiceError = { message: string, code: number; metadata: grpc.Metadata }
+export type Status = { details: string, code: number; metadata: grpc.Metadata }
+
+interface UnaryResponse {
+  cancel(): void;
+}
+interface ResponseStream<T> {
+  cancel(): void;
+  on(type: 'data', handler: (message: T) => void): ResponseStream<T>;
+  on(type: 'end', handler: (status?: Status) => void): ResponseStream<T>;
+  on(type: 'status', handler: (status: Status) => void): ResponseStream<T>;
+}
+interface RequestStream<T> {
+  write(message: T): RequestStream<T>;
+  end(): void;
+  cancel(): void;
+  on(type: 'end', handler: (status?: Status) => void): RequestStream<T>;
+  on(type: 'status', handler: (status: Status) => void): RequestStream<T>;
+}
+interface BidirectionalStream<ReqT, ResT> {
+  write(message: ReqT): BidirectionalStream<ReqT, ResT>;
+  end(): void;
+  cancel(): void;
+  on(type: 'data', handler: (message: ResT) => void): BidirectionalStream<ReqT, ResT>;
+  on(type: 'end', handler: (status?: Status) => void): BidirectionalStream<ReqT, ResT>;
+  on(type: 'status', handler: (status: Status) => void): BidirectionalStream<ReqT, ResT>;
+}
+
+export class EspressoClient {
+  readonly serviceHost: string;
+
+  constructor(serviceHost: string, options?: grpc.RpcOptions);
+  groupHeadTemperature(requestMessage: pkg_espressopb_espresso_pb.TemperatureStreamRequest, metadata?: grpc.Metadata): ResponseStream<pkg_espressopb_espresso_pb.TemperatureStreamResponse>;
+  boilerTemperature(requestMessage: pkg_espressopb_espresso_pb.TemperatureStreamRequest, metadata?: grpc.Metadata): ResponseStream<pkg_espressopb_espresso_pb.TemperatureStreamResponse>;
+  getTargetTemperature(
+    requestMessage: pkg_espressopb_espresso_pb.GetTargetTemperatureRequest,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: pkg_espressopb_espresso_pb.GetTargetTemperatureResponse|null) => void
+  ): UnaryResponse;
+  getTargetTemperature(
+    requestMessage: pkg_espressopb_espresso_pb.GetTargetTemperatureRequest,
+    callback: (error: ServiceError|null, responseMessage: pkg_espressopb_espresso_pb.GetTargetTemperatureResponse|null) => void
+  ): UnaryResponse;
+  setTargetTemperature(
+    requestMessage: pkg_espressopb_espresso_pb.SetTargetTemperatureRequest,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: pkg_espressopb_espresso_pb.SetTargetTemperatureResponse|null) => void
+  ): UnaryResponse;
+  setTargetTemperature(
+    requestMessage: pkg_espressopb_espresso_pb.SetTargetTemperatureRequest,
+    callback: (error: ServiceError|null, responseMessage: pkg_espressopb_espresso_pb.SetTargetTemperatureResponse|null) => void
+  ): UnaryResponse;
+}
+
---
+++ ui/src/proto/pkg/espressopb/espresso_pb_service.js
@@ -0,0 +1,198 @@
+/* eslint-disable */
+// package: espressopb
+// file: pkg/espressopb/espresso.proto
+
+var pkg_espressopb_espresso_pb = require("../../pkg/espressopb/espresso_pb");
+var grpc = require("@improbable-eng/grpc-web").grpc;
+
+var Espresso = (function () {
+  function Espresso() {}
+  Espresso.serviceName = "espressopb.Espresso";
+  return Espresso;
+}());
+
+Espresso.GroupHeadTemperature = {
+  methodName: "GroupHeadTemperature",
+  service: Espresso,
+  requestStream: false,
+  responseStream: true,
+  requestType: pkg_espressopb_espresso_pb.TemperatureStreamRequest,
+  responseType: pkg_espressopb_espresso_pb.TemperatureStreamResponse
+};
+
+Espresso.BoilerTemperature = {
+  methodName: "BoilerTemperature",
+  service: Espresso,
+  requestStream: false,
+  responseStream: true,
+  requestType: pkg_espressopb_espresso_pb.TemperatureStreamRequest,
+  responseType: pkg_espressopb_espresso_pb.TemperatureStreamResponse
+};
+
+Espresso.GetTargetTemperature = {
+  methodName: "GetTargetTemperature",
+  service: Espresso,
+  requestStream: false,
+  responseStream: false,
+  requestType: pkg_espressopb_espresso_pb.GetTargetTemperatureRequest,
+  responseType: pkg_espressopb_espresso_pb.GetTargetTemperatureResponse
+};
+
+Espresso.SetTargetTemperature = {
+  methodName: "SetTargetTemperature",
+  service: Espresso,
+  requestStream: false,
+  responseStream: false,
+  requestType: pkg_espressopb_espresso_pb.SetTargetTemperatureRequest,
+  responseType: pkg_espressopb_espresso_pb.SetTargetTemperatureResponse
+};
+
+exports.Espresso = Espresso;
+
+function EspressoClient(serviceHost, options) {
+  this.serviceHost = serviceHost;
+  this.options = options || {};
+}
+
+EspressoClient.prototype.groupHeadTemperature = function groupHeadTemperature(requestMessage, metadata) {
+  var listeners = {
+    data: [],
+    end: [],
+    status: []
+  };
+  var client = grpc.invoke(Espresso.GroupHeadTemperature, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onMessage: function (responseMessage) {
+      listeners.data.forEach(function (handler) {
+        handler(responseMessage);
+      });
+    },
+    onEnd: function (status, statusMessage, trailers) {
+      listeners.status.forEach(function (handler) {
+        handler({ code: status, details: statusMessage, metadata: trailers });
+      });
+      listeners.end.forEach(function (handler) {
+        handler({ code: status, details: statusMessage, metadata: trailers });
+      });
+      listeners = null;
+    }
+  });
+  return {
+    on: function (type, handler) {
+      listeners[type].push(handler);
+      return this;
+    },
+    cancel: function () {
+      listeners = null;
+      client.close();
+    }
+  };
+};
+
+EspressoClient.prototype.boilerTemperature = function boilerTemperature(requestMessage, metadata) {
+  var listeners = {
+    data: [],
+    end: [],
+    status: []
+  };
+  var client = grpc.invoke(Espresso.BoilerTemperature, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onMessage: function (responseMessage) {
+      listeners.data.forEach(function (handler) {
+        handler(responseMessage);
+      });
+    },
+    onEnd: function (status, statusMessage, trailers) {
+      listeners.status.forEach(function (handler) {
+        handler({ code: status, details: statusMessage, metadata: trailers });
+      });
+      listeners.end.forEach(function (handler) {
+        handler({ code: status, details: statusMessage, metadata: trailers });
+      });
+      listeners = null;
+    }
+  });
+  return {
+    on: function (type, handler) {
+      listeners[type].push(handler);
+      return this;
+    },
+    cancel: function () {
+      listeners = null;
+      client.close();
+    }
+  };
+};
+
+EspressoClient.prototype.getTargetTemperature = function getTargetTemperature(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(Espresso.GetTargetTemperature, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+EspressoClient.prototype.setTargetTemperature = function setTargetTemperature(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(Espresso.SetTargetTemperature, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+exports.EspressoClient = EspressoClient;
+
