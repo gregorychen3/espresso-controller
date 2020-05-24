c7d73c9aadeb54a2c94b6fba67cbcd556a6e3da0
Gregory Chen <gregorychen3@gmail.com>
2020-05-24 12:01:28 -0400

    make cli command shorter

--- cmd/espresso/main.go
+++ cmd/espresso/main.go
@@ -1,47 +1,75 @@
 package main
 
 import (
-	"github.com/gregorychen3/espresso-controller/cmd/espresso/appliance"
 	"github.com/gregorychen3/espresso-controller/cmd/espresso/cmdutil"
 	"github.com/gregorychen3/espresso-controller/cmd/espresso/config"
 	"github.com/gregorychen3/espresso-controller/cmd/espresso/log"
+	"github.com/gregorychen3/espresso-controller/internal/appliance"
+	serverLogger "github.com/gregorychen3/espresso-controller/internal/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
+
+var configKeys = []config.Key{
+	{Path: "Port", ShortFlag: "p", Description: "Port on which the appliance server should listen", Default: "8080"},
+	{Path: "RelayPin", ShortFlag: "r", Description: "The GPIO pin on which the relay is connected", Default: 5},
+	{Path: "BoilerThermCsPin", ShortFlag: "", Description: "The GPIO pin connected to the boiler thermometer's max31855 chip select, aka chip enable", Default: 8},
+	{Path: "BoilerThermClkPin", ShortFlag: "", Description: "The GPIO pin connected to the boiler thermometer's max31855 clock", Default: 11},
+	{Path: "BoilerThermMisoPin", ShortFlag: "", Description: "The GPIO pin connected to the boiler thermometer's max31855 data output", Default: 9},
+	{Path: "Verbose", ShortFlag: "v", Description: "verbose output", Default: false},
+}
 
 func newRootCmd() *cobra.Command {
 	cmd := cobra.Command{
 		Use:   "espresso",
 		Short: "Control and monitor an espresso machine",
 		Long:  "Control and monitor an espresso machine",
-		Run: func(cmd *cobra.Command, args []string) {
-			if err := cmd.Help(); err != nil {
-				log.Fatal("Failed to output help for command")
+		PreRun: func(cmd *cobra.Command, args []string) {
+			// Bind config in PreRun() to avoid collisions with other commands' flags
+			for _, k := range configKeys {
+				if err := viper.BindPFlag(k.Path, cmd.Flags().Lookup(k.Flag())); err != nil {
+					log.Fatal("Failed to bind flag to config: %+v", k)
+				}
 			}
-			log.Info("\nFind more information at https://github.com/gregorychen3/espresso-controller")
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			log.Info("For more information visit https://github.com/gregorychen3/espresso-controller\n%s", cmdutil.Logo)
+
+			if verbose := viper.GetBool("Verbose"); verbose {
+				serverLogger.UseDevLogger()
+			} else {
+				serverLogger.UseProdLogger(
+					viper.GetString(config.KeyLogFilePath),
+					viper.GetInt(config.KeyLogFileMaxSize),
+					viper.GetInt(config.KeyLogFileMaxAge),
+					viper.GetInt(config.KeyLogFileMaxBackups),
+				)
+			}
+
+			c := appliance.Configuration{}
+			if err := viper.Unmarshal(&c); err != nil {
+				log.Fatal("Unmarshalling configuration: %s\n", err.Error())
+			}
+
+			server := appliance.New(c)
+			return server.Run()
 		},
 	}
 
-	pFlags := cmd.PersistentFlags()
-	pFlags.BoolP("verbose", "v", false, "verbose output")
-	viper.BindPFlag(config.KeyLogVerbose, pFlags.Lookup("verbose"))
-
-	cobra.OnInitialize(onInit)
-
-	cmd.AddCommand(appliance.NewApplianceCmd())
-
+	for _, k := range configKeys {
+		if k.Default != nil {
+			viper.SetDefault(k.Path, k.Default)
+		}
+		k.BindFlag(&cmd)
+	}
+	for _, k := range configKeys {
+		viper.BindEnv(k.Path, k.EnvKey())
+	}
 	return &cmd
 }
 
 func main() {
-	log.Info(cmdutil.Logo)
 	if err := newRootCmd().Execute(); err != nil {
 		log.Fatal(err.Error())
-	}
-}
-
-func onInit() {
-	if verbose := viper.GetBool(config.KeyLogVerbose); verbose {
-		log.SetVerbose()
 	}
 }
