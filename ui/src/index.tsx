432854e811a59e63dba9636444b73ff7bd72e6eb
Gregory Chen <gregorychen3@gmail.com>
2020-05-25 18:58:14 -0400

    remove unused imjport

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-import "./index.css";
 import { store } from "./redux";
 import * as serviceWorker from "./serviceWorker";
 
