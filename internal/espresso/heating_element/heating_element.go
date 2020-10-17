3de2ff0c0df0ad04558cd97c9a7445b5d7fae5e4
Gregory Chen <gregorychen3@gmail.com>
2020-10-16 23:38:28 -0400

    pwm heating element

--- internal/espresso/heating_element/heating_element.go
+++ internal/espresso/heating_element/heating_element.go
@@ -1,6 +1,41 @@
 package heating_element
 
-type HeatingElement interface {
-	HeatOn()
-	HeatOff()
+import (
+	"time"
+
+	"github.com/stianeikeland/go-rpio/v4"
+)
+
+type HeatingElement struct {
+	relayPin   rpio.Pin
+	dutyFactor float32
+}
+
+func NewHeatingElement(relayPinNum int) *HeatingElement {
+	relayPin := rpio.Pin(relayPinNum)
+	relayPin.Output()
+
+	return &HeatingElement{
+		relayPin:   relayPin,
+		dutyFactor: 0,
+	}
+}
+
+func (h *HeatingElement) Run() {
+	go func() {
+		for {
+			h.relayPin.High()
+			ms := int(h.dutyFactor * 1000)
+			time.Sleep(time.Duration(ms) * time.Millisecond)
+			h.relayPin.Low()
+		}
+	}()
+}
+
+func (h *HeatingElement) SetDutyFactor(factor float32) {
+	h.dutyFactor = factor
+}
+
+func (h *HeatingElement) Shutdown() {
+	h.relayPin.Low()
 }
