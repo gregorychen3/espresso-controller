a6beeda0af2678efdc2d44109f252934ffddbb6a
Gregory Chen <gregorychen3@gmail.com>
2020-10-18 10:40:37 -0400

    imports

--- ui/src/components/Title.tsx
+++ ui/src/components/Title.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import PropTypes from "prop-types";
 import Typography from "@material-ui/core/Typography";
+import React from "react";
 
 interface Props {
   children: React.ReactNode;
