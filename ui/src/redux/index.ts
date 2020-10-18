603ba296029b3668156ef0376962c685d41ea4fa
Gregory Chen <gregorychen3@gmail.com>
2020-10-18 13:09:35 -0400

    remove serializable check middleware

--- ui/src/redux/index.ts
+++ ui/src/redux/index.ts
@@ -1,18 +1,19 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { boilerTemperatureSlice } from "./boilerTemperatureSlice";
 import { configurationSlice } from "./configurationSlice";
 import { uiSlice } from "./uiSlice";
 
-const rootReducer = combineReducers({
-  boilerTemperature: boilerTemperatureSlice.reducer,
-  configuration: configurationSlice.reducer,
-  ui: uiSlice.reducer,
-});
-
-export default rootReducer;
-
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    boilerTemperature: boilerTemperatureSlice.reducer,
+    configuration: configurationSlice.reducer,
+    ui: uiSlice.reducer,
+  },
+  middleware: getDefaultMiddleware({
+    thunk: true,
+    immutableCheck: true,
+    serializableCheck: false,
+  }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
