f95ec09b2def4a8433a33aad3b796663a56f1795
embeddeddev88 <danielbergmann1@gmail.com>
2023-01-07 13:15:12 +0000

    changing to from adafruit library to sparkfun and other refactoring details

--- pkg/oled/grpc_client_micro_oled.py
+++ pkg/oled/grpc_client_micro_oled.py
@@ -1,18 +1,19 @@
+#!/usr/bin/env python3
 """
-This demo will fill the screen with white, draw a black box on top
-and then print Hello World! in the center of the display
-
-This example is for use on (Linux) computers that are using CPython with
-Adafruit Blinka to support CircuitPython libraries. CircuitPython does
-not support PIL/pillow (python imaging library)!
-
 We are using Sparkfun micro OLED breakout (Qwiic), article LCD-14532
+
+Prereqs:
+* install DSEG font: sudo apt-get install fonts-dseg
+* all packages need to be installed including luma
+
 """
 
 import board
 import digitalio
 from PIL import Image, ImageDraw, ImageFont
-import adafruit_ssd1306
+from luma.core.interface.serial import i2c
+from luma.core.render import canvas
+from luma.oled.device import ssd1306
 import grpc
 import espresso_pb2
 import espresso_pb2_grpc
@@ -21,67 +22,21 @@ import os
 import subprocess
 import sys, time
 
-# Define the Reset Pin
-oled_reset = digitalio.DigitalInOut(board.D4)
-
-# Change these
-# to the right size for your display!
-WIDTH = 64
-HEIGHT = 48  # Change to 64 if needed
-BORDER = 0
-
 # Use for I2C.
-i2c = board.I2C()
-oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3D, reset=oled_reset)
-
-# Use for SPI
-# spi = board.SPI()
-# oled_cs = digitalio.DigitalInOut(board.D5)
-# oled_dc = digitalio.DigitalInOut(board.D6)
-# oled = adafruit_ssd1306.SSD1306_SPI(WIDTH, HEIGHT, spi, oled_dc, oled_reset, oled_cs)
-
-# Clear display.
-oled.fill(0)
-oled.show()
-
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-image = Image.new("1", (oled.width, oled.height))
-
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
-
-# Draw a white background
-draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
-
-# Draw a smaller inner rectangle
-draw.rectangle(
-    (BORDER, BORDER, oled.width - BORDER - 1, oled.height - BORDER - 1),
-    outline=0,
-    fill=0,
-)
-# Load default font.
-#font = ImageFont.load_default()
-font = ImageFont.truetype("DejaVuSans-Oblique.ttf", size=15)
+serial = i2c(port=1, address=0x3D)
+device = ssd1306(serial, width=64, height=48, rotate=2)
+font = ImageFont.truetype("LiberationSerif-Bold.ttf", size=15)
 
 def print_to_oled( temperature ):
-    # Create blank image for drawing.
-    # Make sure to create image with mode '1' for 1-bit color.
-    image = Image.new("1", (oled.width, oled.height))
-    # Get drawing object to draw on image.
-    draw = ImageDraw.Draw(image)
-    # Clear display.
-    oled.fill(0)
     text = str(temperature)+"°C"
     (font_width, font_height) = font.getsize(str(text))
-    draw.text(
-        (oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2),
-              text,
-              font=font,
-              fill=255,
-    )
-    oled.image(image)
-    oled.show()
+    with canvas(device) as draw:
+        draw.rectangle(device.bounding_box, outline="white", fill="black")
+        draw.text((device.width // 2 - font_width // 2, device.height // 2 - font_height // 2),
+                text,
+                font=font,
+                fill="white",
+        )
 
 def run():
    with grpc.insecure_channel('localhost:8080') as channel:
